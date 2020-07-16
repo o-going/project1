a899a23c7b3be7e3f85764796562be0e0f05f227
dhrkfl9810@gmail.com <dhrkfl9810@gmail.com>
2020-07-16 16:27:47 +0900

    practice

---
+++ phpjs/3.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <script>
+        document.write('hello world');
+    </script>
+    <?php
+        
+    ?>
+</body>
+</html>
