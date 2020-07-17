ccab7f7fcab9b372526262660feb3eab4b141050
dhrkfl9810@gmail.com <dhrkfl9810@gmail.com>
2020-07-17 20:54:58 +0900

    php login form html

---
+++ phpjs/8_1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <form action="8_2.php">
+        <p>비밀번호를 입력해주세요.</p>
+        <input type="password" name="password">
+        <input type="submit">
+    </form>
+</body>
+</html>
