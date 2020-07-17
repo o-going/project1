280399ebf95a1f8cdc633ec585295aea0284aae2
dhrkfl9810@gmail.com <dhrkfl9810@gmail.com>
2020-07-17 20:47:44 +0900

    variable

---
+++ phpjs/4.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <h1>JavaScript</h1>
+    <script>
+        let name = 'egoing';
+        document.write('안녕하세요 ' + name);
+    </script>
+    <h1>php</h1>
+    <?php
+        $name = 'egoing';
+        echo '안녕하세요 '.$name;
+    ?>
+</body>
+</html>
