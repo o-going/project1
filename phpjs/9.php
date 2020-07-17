c1c855aa782e0962d0c63497d4ff76c8cc478ea5
dhrkfl9810@gmail.com <dhrkfl9810@gmail.com>
2020-07-17 20:56:40 +0900

    while

---
+++ phpjs/9.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <h1>JavaScript</h1>
+    <ul>
+    <script>
+        i = 0;
+        while(i < 10){
+            document.write('<li>hello world</li>');
+            i = i + 1;
+        } 
+    </script>
+    </ul>
+
+    <h1>php</h1>
+    <ul>
+    <?php
+        $i = 0;
+        while($i < 10){
+            echo '<li>hello world</li>';
+            $i = $i + 1;
+        }
+    ?>
+    </ul>
+</body>
+</html>
