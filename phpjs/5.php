0c5be751d90b7859fa8504b35caa692089247952
dhrkfl9810@gmail.com <dhrkfl9810@gmail.com>
2020-07-17 20:51:43 +0900

    practice

---
+++ phpjs/5.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <h1>JavaScript</h1>
+    <script>
+        document.write(2===1);
+    </script>
+    <h1>php</h1>
+    <?php
+        var_dump(2===1);
+    ?>
+</body>
+</html>
