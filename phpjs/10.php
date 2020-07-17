f2e8d6be431a27110b13c5740476143ea377ce83
dhrkfl9810@gmail.com <dhrkfl9810@gmail.com>
2020-07-17 20:57:53 +0900

    array

---
+++ phpjs/10.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <h1>JavaScript</h1>
+    <script>
+        let list = new Array('one', 'two', 'three');
+        document.write(list[2]);
+        document.write(list.length);
+    </script>
+
+    <h1>php</h1>
+    <?php
+        $list = array('one', 'two', 'three');
+        echo $list[1];
+        echo count($list);
+    ?>
+</body>
+</html>
