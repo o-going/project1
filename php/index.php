165160b3689bc00a951c5d6762a9c0a7c57aa48d
dhrkfl9810@gmail.com <dhrkfl9810@gmail.com>
2020-07-22 17:27:00 +0900

    index.php

--- php/index.php
+++ php/index.php
@@ -1,6 +1,7 @@
 <?php
-    $conn = mysqli_connect('localhost','root','rkdud4578');
+    $conn = mysqli_connect('localhost','root','password');
     mysqli_select_db($conn, 'opentutorials');
+    $result = mysqli_query($conn, 'SELECT * FROM topic');
 ?>
 
 <!DOCTYPE html>
@@ -17,18 +18,25 @@
     <nav>
         <ol>
             <?php
-                echo file_get_contents('list.txt');
+                while($row = mysqli_fetch_assoc($result)){
+                    echo "<li><a href='http://localhost/php/index.php?id=".$row['id']."'>".$row['title']."</a></li>"."\n";
+                }
             ?>
         </ol>
     </nav>
     <div id="control">
         <input type="button" value="white" onclick="document.getElementById('target').className = 'white'"/>
         <input type="button" value="black" onclick="document.getElementById('target').className = 'black'"/>
+        <a href="http://localhost/php/write.php">쓰기</a>
     </div>
     <article>
         <?php
-            if(empty($_GET['id']) == false){
-            echo file_get_contents($_GET['id'].'.txt');
+            if(empty($_GET['id']) === false){
+            $sql = 'SELECT * FROM topic WHERE id='.$_GET['id'];
+            $result = mysqli_query($conn, $sql);
+            $row = mysqli_fetch_assoc($result);
+            echo '<h2>'.$row['title'].'</h2>';
+            echo $row['description'];
             }
         ?>
     </article>
