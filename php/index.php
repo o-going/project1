2fdf5d851e2aac45c30fd088e2825ab3b4a2995f
dhrkfl9810@gmail.com <dhrkfl9810@gmail.com>
2020-07-23 23:39:23 +0900

    index.php

--- php/index.php
+++ php/index.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = mysqli_connect('localhost','root','password');
+    $conn = mysqli_connect('localhost','root','rkdud4578');
     mysqli_select_db($conn, 'opentutorials');
     $result = mysqli_query($conn, 'SELECT * FROM topic');
 ?>
@@ -32,10 +32,11 @@
     <article>
         <?php
             if(empty($_GET['id']) === false){
-            $sql = 'SELECT * FROM topic WHERE id='.$_GET['id'];
+            $sql = "SELECT topic.id,title,name,description FROM topic LEFT JOIN user ON topic.author = user.id WHERE topic.id=".$_GET['id'];
             $result = mysqli_query($conn, $sql);
             $row = mysqli_fetch_assoc($result);
             echo '<h2>'.$row['title'].'</h2>';
+            echo '<p>'.$row['name'].'</p>';
             echo $row['description'];
             }
         ?>
