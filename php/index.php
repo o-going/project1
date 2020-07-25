a86c04e2cfe2a6a3076a60c7ceced8622763d57b
o-going <dhrkfl9810@gmail.com>
2020-07-25 14:52:30 +0900

    index.php

--- php/index.php
+++ php/index.php
@@ -1,45 +1,45 @@
 <?php
-    $conn = mysqli_connect('localhost','root',1111);
-    mysqli_select_db($conn, 'opentutorials');
-    $result = mysqli_query($conn, 'SELECT * FROM topic');
+require("../config/config.php");
+require("../lib/db.php");
+$conn = db_init($config["host"], $config["duser"], $config["dpw"], $config["dname"]);
+$result = mysqli_query($conn, "SELECT * FROM topic");
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="http://localhost/css/style.css">
+     <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="http://localhost/css/style.css">
 </head>
 <body id="target">
     <header>
     <img src="https://s3.ap-northeast-2.amazonaws.com/opentutorials-user-file/course/94.png" alt="생활코딩">
         <h1><a href="http://localhost/php/index.php">JavaScript</a></h1>
-    </header>
+  </header>
     <nav>
         <ol>
-            <?php
-                while($row = mysqli_fetch_assoc($result)){
-                    echo "<li><a href='http://localhost/php/index.php?id=".$row['id']."'>".htmlspecialchars($row['title'])."</a></li>"."\n";
-                }
-            ?>
+    <?php
+        while($row = mysqli_fetch_assoc($result)){
+        echo '<li><a href="http://localhost/php/index.php?id='.$row['id'].'">'.htmlspecialchars($row['title']).'</a></li>'."\n";
+    }
+    ?>
         </ol>
     </nav>
-    <div id="control">
-        <input type="button" value="white" onclick="document.getElementById('target').className = 'white'"/>
-        <input type="button" value="black" onclick="document.getElementById('target').className = 'black'"/>
-        <a href="http://localhost/php/write.php">쓰기</a>
-    </div>
-    <article>
-        <?php
-            if(empty($_GET['id']) === false){
-            $sql = "SELECT topic.id,title,name,description FROM topic LEFT JOIN user ON topic.author = user.id WHERE topic.id=".$_GET['id'];
-            $result = mysqli_query($conn, $sql);
-            $row = mysqli_fetch_assoc($result);
-            echo '<h2>'.htmlspecialchars($row['title']).'</h2>';
-            echo '<p>'.htmlspecialchars($row['name']).'</p>';
-            echo strip_tags($row['description'], '<a><h1><h2><h3><h4><h5><ul><ol><li>'); // 사용한 태그들만 허용
-            }
-        ?>
-    </article>
+  <div id="control">
+    <input type="button" value="white" onclick="document.getElementById('target').className='white'"/>
+    <input type="button" value="black" onclick="document.getElementById('target').className='black'" />
+    <a href="http://localhost/php/write.php">쓰기</a>
+  </div>
+  <article>
+  <?php
+  if(empty($_GET['id']) === false ) {
+      $sql = "SELECT topic.id,title,name,description FROM topic LEFT JOIN user ON topic.author = user.id WHERE topic.id=".$_GET['id'];
+      $result = mysqli_query($conn, $sql);
+      $row = mysqli_fetch_assoc($result);
+      echo '<h2>'.htmlspecialchars($row['title']).'</h2>';
+      echo '<p>'.htmlspecialchars($row['name']).'</p>';
+      echo strip_tags($row['description'], '<a><h1><h2><h3><h4><h5><ul><ol><li>');
+  }
+  ?>
+  </article>
 </body>
 </html>
